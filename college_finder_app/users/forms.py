7d93e3b3746769cd3674cf7525a4009739e5f66f
Madan Neupane <madanneupane09@gmail.com>
2021-01-10 23:31:30 +0545

    adding Profile Update Form

--- college_finder_app/users/forms.py
+++ college_finder_app/users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from .models import Profile
+
 
 class UserRegisterForm(forms.Form):
     fullname = forms.CharField(max_length=32)
-    email = forms.EmailField()    
+    email = forms.EmailField()
     reg_username = forms.CharField(max_length=20)
     reg_password = forms.CharField(widget=forms.PasswordInput)
     reg_password_repeat = forms.CharField(widget=forms.PasswordInput)
@@ -11,3 +13,9 @@ class UserRegisterForm(forms.Form):
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=20)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class ProfileUpdateForm(forms.Form):
+    model = Profile
+    field = ['bio', 'avatar', 'country', 'gpa', 'gre_score',
+             'toefl_score', 'sop_score', 'lor_score', 'uni_score', 'research']
