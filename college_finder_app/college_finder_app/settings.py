153b19dfb7fb0bc03ee178a0b7095ef69aeb49e5
Madan Neupane <madanneupane09@gmail.com>
2021-01-12 23:18:49 +0545

    added avatar

--- college_finder_app/college_finder_app/settings.py
+++ college_finder_app/college_finder_app/settings.py
@@ -146,7 +146,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     Path(BASE_DIR) / "components" / "static"
 ]
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'components/media')
+MEDIA_URL = '/media/'
 
 load_dotenv()
 
