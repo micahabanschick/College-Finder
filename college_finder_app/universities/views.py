d1c349c2a8e5f7785a2a6e864ca225ae05a95d3e
Madan Neupane <madanneupane09@gmail.com>
2021-01-13 20:11:15 +0545

    added search functionality

--- college_finder_app/universities/views.py
+++ college_finder_app/universities/views.py
@@ -2,19 +2,28 @@ from django.core import paginator
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Universities
-
-data = Universities.objects.all()
+from django.db.models import Q
 
 
 def universities_page(request):
+
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        data = Universities.objects.filter(
+            Q(name__icontains=search_query) | Q(location__icontains=search_query))
+    else:
+        data = Universities.objects.all()
+
     paginator = Paginator(data, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
 
     context = {
         'title': 'Universities',
         'universities': page_obj,
         'page_obj': page_obj,
+        'search_query': search_query,
     }
     return render(request, 'universities/universities.html', context)
 
