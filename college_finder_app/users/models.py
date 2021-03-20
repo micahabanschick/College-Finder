00ca6532b9fc7dc85a61e41197b8140a99d48a84
Madan Neupane <madanneupane09@gmail.com>
2021-03-20 17:35:52 +0545

    image upload resize

--- college_finder_app/users/models.py
+++ college_finder_app/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django import forms
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -21,3 +21,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
