1c3b9700f0a8d23768d353ddc4d838ba0cbaa772
Madan Neupane <madanneupane09@gmail.com>
2021-05-17 23:12:32 +0545

    Revert "test"
    This reverts commit 7f5d1826002a162b16214e53fcf61dedb4b80203.

--- college_finder_app/settings.py
+++ college_finder_app/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'components/static'),
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
