04a56a3af1c3d27e55b1404cf3d801ee08f61638
Madan Neupane <madanneupane09@gmail.com>
2021-05-19 14:39:51 +0545

    Revert "minor changes"
    This reverts commit 0750154d458951794e30f0102259931bdf56d802.

--- college_finder_app/settings.py
+++ college_finder_app/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-load_dotenv()
-DEBUG = os.environ['DEBUG_MODE']
+DEBUG = True
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
