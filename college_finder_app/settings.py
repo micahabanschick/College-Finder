93a9f8d080049cd7902597d556fb13c36099471b
Madan Neupane <madanneupane09@gmail.com>
2021-05-21 22:31:08 +0545

    Revert "changing staticfiles_storage"
    This reverts commit a9eff5db371da7326c75c92a3dca30db483b2a08.

--- college_finder_app/settings.py
+++ college_finder_app/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'components/static'),
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
