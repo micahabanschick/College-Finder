62cae5927173dcfd9dd809e9a134f8d72115da56
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 01:14:23 -0400

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
