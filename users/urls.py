156f4983e39177f39aee9c446ca667197d064c57
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 11:28:02 -0400

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('register', views.RegistrationView.as_view(), name='register'),
-    path('', views.LoginView.as_view(), name='login'),
-    path('logout', views.logout_page, name='logout'),
-    path('activate/<uidb64>/<token>',
-         views.ActivateAccountView.as_view(), name='activate'),
-    path('settings', login_required(
-        views.profile_update_form), name='update_profile'),
-    path('forgot-password', views.forgot_password_page,
-         name='forgot-password'),
-    path('reset-password/<uidb64>/<token>',
-         views.reset_password_page, name='reset-password'),
+    # path('register', views.RegistrationView.as_view(), name='register'),
+    # path('', views.LoginView.as_view(), name='login'),
+    # path('logout', views.logout_page, name='logout'),
+    # path('activate/<uidb64>/<token>',
+    #      views.ActivateAccountView.as_view(), name='activate'),
+    # path('settings', login_required(
+    #     views.profile_update_form), name='update_profile'),
+    # path('forgot-password', views.forgot_password_page,
+    #      name='forgot-password'),
+    # path('reset-password/<uidb64>/<token>',
+    #      views.reset_password_page, name='reset-password'),
 ]
