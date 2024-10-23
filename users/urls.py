d0fe54f511bd0aa28a915a179c6fae8b626ad172
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 00:52:08 -0400

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    # path('register', views.RegistrationView.as_view(), name='register'),
-    # path('', views.LoginView.as_view(), name='login'),
-    # path('logout', views.logout_page, name='logout'),
+    path('register', views.RegistrationView.as_view(), name='register'),
+    path('', views.LoginView.as_view(), name='login'),
+    path('logout', views.logout_page, name='logout'),
     path('activate/<uidb64>/<token>',
          views.ActivateAccountView.as_view(), name='activate'),
     path('settings', login_required(
