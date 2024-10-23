c20245c6ff3d3346cad7298421a37d6ad32ddaa6
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 00:08:05 -0400

    Update settings.py

--- college_finder_app/settings.py
+++ college_finder_app/settings.py
@@ -189,13 +189,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Sentry
 sentry_sdk.init(
-    dsn=os.getenv('dsn'),
+    dsn=os.getenv('SENTRY_DSN', ''),
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production,
-    traces_sample_rate=0,
+    traces_sample_rate=1.0,
 
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
