13a5f2221b134a311488bd7777b4e9b1d683906a
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 11:30:27 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.db.utils import ConnectionDoesNotExist
-from django.shortcuts import redirect, render
+# from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib import messages
 from validate_email import validate_email
@@ -24,185 +24,185 @@ username_pattern = re.compile(
     r'^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$')
 
 
-class RegistrationView(View):
+# class RegistrationView(View):
     
-    def get(self, request):
-        return redirect('dashboard')
-        # if request.user.is_authenticated:
-        #     return redirect('dashboard')
-        # return render(request, 'users/login.html', context={'mode': 'signup', })
+#     def get(self, request):
+#         return redirect('dashboard')
+#         # if request.user.is_authenticated:
+#         #     return redirect('dashboard')
+#         # return render(request, 'users/login.html', context={'mode': 'signup', })
 
-    def post(self, request):
-        return redirect('dashboard')
-        # has_error = False
-        # reg_form = request.POST
+#     def post(self, request):
+#         return redirect('dashboard')
+#         # has_error = False
+#         # reg_form = request.POST
 
-        # fullname = request.POST.get('fullname')
-        # email = request.POST.get('email')
-        # reg_username = request.POST.get('reg_username')
-        # reg_password = request.POST.get('reg_password')
-        # reg_password_repeat = request.POST.get('reg_password_repeat')
+#         # fullname = request.POST.get('fullname')
+#         # email = request.POST.get('email')
+#         # reg_username = request.POST.get('reg_username')
+#         # reg_password = request.POST.get('reg_password')
+#         # reg_password_repeat = request.POST.get('reg_password_repeat')
 
-        # if not validate_email(email):
-        #     messages.add_message(request, messages.ERROR,
-        #                          'Please provide a valid email.')
-        #     has_error = True
+#         # if not validate_email(email):
+#         #     messages.add_message(request, messages.ERROR,
+#         #                          'Please provide a valid email.')
+#         #     has_error = True
 
-        # try:
-        #     if User.objects.get(email=email):
-        #         messages.add_message(request, messages.ERROR,
-        #                              'Email is already taken.')
-        #         has_error = True
-        # except Exception as e:
-        #     pass
+#         # try:
+#         #     if User.objects.get(email=email):
+#         #         messages.add_message(request, messages.ERROR,
+#         #                              'Email is already taken.')
+#         #         has_error = True
+#         # except Exception as e:
+#         #     pass
 
-        # try:
-        #     if User.objects.get(reg_username=reg_username):
-        #         messages.add_message(request, messages.ERROR,
-        #                              'Username is already taken.')
-        #         has_error = True
-        # except Exception as e:
-        #     pass
+#         # try:
+#         #     if User.objects.get(reg_username=reg_username):
+#         #         messages.add_message(request, messages.ERROR,
+#         #                              'Username is already taken.')
+#         #         has_error = True
+#         # except Exception as e:
+#         #     pass
 
-        # if len(reg_username) < 5 or 20 < len(reg_username):
-        #     messages.add_message(
-        #         request, messages.ERROR, 'Username must be between 5 and 20 characters.')
-        #     has_error = True
+#         # if len(reg_username) < 5 or 20 < len(reg_username):
+#         #     messages.add_message(
+#         #         request, messages.ERROR, 'Username must be between 5 and 20 characters.')
+#         #     has_error = True
 
-        # if not is_safe_username(reg_username):
-        #     messages.add_message(
-        #         request, messages.WARNING, f'"{reg_username}" is not allowed for username.')
-        #     has_error = True
+#         # if not is_safe_username(reg_username):
+#         #     messages.add_message(
+#         #         request, messages.WARNING, f'"{reg_username}" is not allowed for username.')
+#         #     has_error = True
 
-        # if not username_pattern.match(reg_username):
-        #     messages.add_message(
-        #         request, messages.ERROR, 'Username can only contain letters (a-z), numbers (0-9), underscores (_), and periods (.)')
-        #     has_error = True
+#         # if not username_pattern.match(reg_username):
+#         #     messages.add_message(
+#         #         request, messages.ERROR, 'Username can only contain letters (a-z), numbers (0-9), underscores (_), and periods (.)')
+#         #     has_error = True
 
-        # if len(reg_password) < 8:
-        #     messages.add_message(
-        #         request, messages.ERROR, 'Password must not be less than 8 characters.')
-        #     has_error = True
+#         # if len(reg_password) < 8:
+#         #     messages.add_message(
+#         #         request, messages.ERROR, 'Password must not be less than 8 characters.')
+#         #     has_error = True
 
-        # if reg_password != reg_password_repeat:
-        #     messages.add_message(request, messages.ERROR,
-        #                          'Password does not match.')
-        #     has_error = True
+#         # if reg_password != reg_password_repeat:
+#         #     messages.add_message(request, messages.ERROR,
+#         #                          'Password does not match.')
+#         #     has_error = True
 
-        # if not has_error:
-        #     try:
-        #         user = User.objects.create(email=email, username=reg_username)
-        #         user.set_password(reg_password)
-        #         user.first_name = fullname.split()[0]
-        #         user.last_name = " ".join(fullname.split()[1:])
-        #         user.is_active = False
-        #         user.save()
+#         # if not has_error:
+#         #     try:
+#         #         user = User.objects.create(email=email, username=reg_username)
+#         #         user.set_password(reg_password)
+#         #         user.first_name = fullname.split()[0]
+#         #         user.last_name = " ".join(fullname.split()[1:])
+#         #         user.is_active = False
+#         #         user.save()
 
-        #         current_site = get_current_site(request)
-        #         email_subject = 'Activate your College Finder account'
-        #         message = get_template('users/activate.html').render(context={
-        #             'user': user,
-        #             'domain': current_site.domain,
-        #             'user_id': urlsafe_base64_encode(force_bytes(user.pk)),
-        #             'token': generate_token.make_token(user)
-        #         })
+#         #         current_site = get_current_site(request)
+#         #         email_subject = 'Activate your College Finder account'
+#         #         message = get_template('users/activate.html').render(context={
+#         #             'user': user,
+#         #             'domain': current_site.domain,
+#         #             'user_id': urlsafe_base64_encode(force_bytes(user.pk)),
+#         #             'token': generate_token.make_token(user)
+#         #         })
 
-        #         email_message = EmailMessage(
-        #             email_subject,
-        #             message,
-        #             settings.DEFAULT_FROM_EMAIL,
-        #             [email],
-        #         )
-        #         email_message.content_subtype = 'html'
-        #         messages.add_message(request, messages.INFO,
-        #                              'Please wait while we process the request.')
-        #         email_message.send()
+#         #         email_message = EmailMessage(
+#         #             email_subject,
+#         #             message,
+#         #             settings.DEFAULT_FROM_EMAIL,
+#         #             [email],
+#         #         )
+#         #         email_message.content_subtype = 'html'
+#         #         messages.add_message(request, messages.INFO,
+#         #                              'Please wait while we process the request.')
+#         #         email_message.send()
 
-        #         messages.add_message(request, messages.SUCCESS,
-        #                              'Account created successfully. Check your email to activate your account.')
-        #         return redirect('login')
+#         #         messages.add_message(request, messages.SUCCESS,
+#         #                              'Account created successfully. Check your email to activate your account.')
+#         #         return redirect('login')
 
-        #     except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError, ConnectionDoesNotExist, ConnectionResetError):
-        #         messages.add_message(request, messages.ERROR,
-        #                              'There was connection problems when signing up your account. Please try again with stable connection.')
-        #         has_error = True
-        #         return redirect('register')
+#         #     except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError, ConnectionDoesNotExist, ConnectionResetError):
+#         #         messages.add_message(request, messages.ERROR,
+#         #                              'There was connection problems when signing up your account. Please try again with stable connection.')
+#         #         has_error = True
+#         #         return redirect('register')
 
-        #     except IntegrityError as e:
-        #         messages.add_message(request, messages.ERROR,
-        #                              'Username is already taken.')
-        #         has_error = True
-        #         return redirect('register')
+#         #     except IntegrityError as e:
+#         #         messages.add_message(request, messages.ERROR,
+#         #                              'Username is already taken.')
+#         #         has_error = True
+#         #         return redirect('register')
 
-        # else:
-        #     has_error = True
-        #     return render(request, 'users/login.html', status=400, context={'mode': 'signup', 'reg_form': reg_form, 'error': has_error})
-
-
-class LoginView(View):
-    def get(self, request):
-        return redirect('dashboard')
-
-    def post(self, request):
-        return redirect('dashboard')
-        # context = {
-        #     'form': request.POST,
-        #     'has_error': False
-        # }
-        # username = request.POST.get('username')
-        # password = request.POST.get('password')
-
-        # if username == '':
-        #     messages.add_message(request, messages.ERROR,
-        #                          'Username is required.')
-        #     context['has_error'] = True
-
-        # if password == '':
-        #     messages.add_message(request, messages.ERROR,
-        #                          'Password is required.')
-        #     context['has_error'] = True
-
-        # user = authenticate(request, username=username, password=password)
-
-        # #if request.user.is_active == False:
-        #  #    messages.add_message(
-        #   #      request, messages.INFO, 'You have not activated your account yet. Please check your email and verify your account first.')
-
-        # if not user and not context['has_error']:
-        #     messages.add_message(request, messages.ERROR,
-        #                          'Invalid Log In credentials.')
-        #     context['has_error'] = True
-
-        # if context['has_error']:
-        #     return render(request, 'users/login.html', status=401, context=context)
-
-        # login(request, user)
-        # messages.add_message(request, messages.INFO,
-        #                      'This is a beta version of the application. Some features are compromised to settle deployment issues.')
-        # return redirect('dashboard')
+#         # else:
+#         #     has_error = True
+#         #     return render(request, 'users/login.html', status=400, context={'mode': 'signup', 'reg_form': reg_form, 'error': has_error})
 
 
-class ActivateAccountView(View):
-    def get(self, request, uidb64, token):
-        try:
-            user_id = force_text(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(pk=user_id)
-        except Exception as identifier:
-            user = None
+# class LoginView(View):
+#     def get(self, request):
+#         return redirect('dashboard')
 
-        if user is not None and generate_token.check_token(user, token):
-            user.is_active = True
-            user.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 'Account activated successfully. You can sign in to the app with your credentials now.')
-            return render(request, 'users/login.html', context={'mode': 'signin'})
-        return render(request, 'users/login.html', context={'mode': 'signup'}, status=401)
+#     def post(self, request):
+#         return redirect('dashboard')
+#         # context = {
+#         #     'form': request.POST,
+#         #     'has_error': False
+#         # }
+#         # username = request.POST.get('username')
+#         # password = request.POST.get('password')
+
+#         # if username == '':
+#         #     messages.add_message(request, messages.ERROR,
+#         #                          'Username is required.')
+#         #     context['has_error'] = True
+
+#         # if password == '':
+#         #     messages.add_message(request, messages.ERROR,
+#         #                          'Password is required.')
+#         #     context['has_error'] = True
+
+#         # user = authenticate(request, username=username, password=password)
+
+#         # #if request.user.is_active == False:
+#         #  #    messages.add_message(
+#         #   #      request, messages.INFO, 'You have not activated your account yet. Please check your email and verify your account first.')
+
+#         # if not user and not context['has_error']:
+#         #     messages.add_message(request, messages.ERROR,
+#         #                          'Invalid Log In credentials.')
+#         #     context['has_error'] = True
+
+#         # if context['has_error']:
+#         #     return render(request, 'users/login.html', status=401, context=context)
+
+#         # login(request, user)
+#         # messages.add_message(request, messages.INFO,
+#         #                      'This is a beta version of the application. Some features are compromised to settle deployment issues.')
+#         # return redirect('dashboard')
 
 
-def logout_page(request):
-    logout(request)
-    messages.add_message(request, messages.INFO, 'Logged out.')
-    return redirect('login')
+# class ActivateAccountView(View):
+#     def get(self, request, uidb64, token):
+#         try:
+#             user_id = force_text(urlsafe_base64_decode(uidb64))
+#             user = User.objects.get(pk=user_id)
+#         except Exception as identifier:
+#             user = None
+
+#         if user is not None and generate_token.check_token(user, token):
+#             user.is_active = True
+#             user.save()
+#             messages.add_message(request, messages.SUCCESS,
+#                                  'Account activated successfully. You can sign in to the app with your credentials now.')
+#             return render(request, 'users/login.html', context={'mode': 'signin'})
+#         return render(request, 'users/login.html', context={'mode': 'signup'}, status=401)
+
+
+# def logout_page(request):
+#     logout(request)
+#     messages.add_message(request, messages.INFO, 'Logged out.')
+#     return redirect('login')
 
 
 def profile_update_form(request):
