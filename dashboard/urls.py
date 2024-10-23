0c38efc2e0c9ed219c904983bf17c84243d6a2bb
Micah Banschick <57916602+micahabanschick@users.noreply.github.com>
2024-10-23 00:54:45 -0400

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', login_required(views.dashboard_page), name='dashboard')
+    path('', name='dashboard')
 ]
